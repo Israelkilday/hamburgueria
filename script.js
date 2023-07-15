2606ea6e69f84f124e66be463d47d1c20b13727d
Israel Kilday <israel.kilday@yahoo.com.br>
2023-07-14 23:05:35 -0300

    modifing script

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 const menu = document.querySelector(".menu");
 const navMenu = document.querySelector(".nav_menu")
 
+menu.addEventListener("click", () => {
+    menu.classList.toggle("ativo");
+    navMenu.classList.toggle("ativo");
+})
