a3ffd9fe8c1f16ffe64ffb82a9eca64021d28cc5
Israel Kilday <israel.kilday@yahoo.com.br>
2023-07-17 11:37:34 -0300

    finalizing project

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const menu = document.querySelector(".menu");
-const NavMenu = document.querySelector(".nav_menu");
+// const menu1 = document.querySelector(".menu2");
+// const NavMenu = document.querySelector(".nav_menu");
 
-menu.addEventListener("click", () => {
-    menu.classList.toggle("ativo");
-    NavMenu.classList.toggle("ativo");
-})
+// menu1.addEventListener("click", () => {
+//     menu1.classList.toggle("ativo");
+//     NavMenu.classList.toggle("ativo");
+// })
